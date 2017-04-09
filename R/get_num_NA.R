69601109c3339e536f34f037177f291f27842477
will pelham <wp8000@gmail.com>
2017-04-09 16:25:52 -0700

    add get_num_NA

---
+++ R/get_num_NA.R
@@ -0,0 +1,16 @@
+#' get_num_NA
+#'
+#' Function to return a dataframe with description of freuqency of NAs
+#' in target dataframe.
+#'
+#' @param data Dataframe.
+#' @export
+#' @examples get_num_NA(df)
+
+#**********************************************************
+get_num_NA = function(data) {
+    tibble::data_frame(variable = names(data),
+                       count.NA = colSums(is.na(data)),
+                       prop.NA = colMeans(is.na(data)) %>% round(2))
+}
+#**********************************************************
