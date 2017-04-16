7548bc9037eff1871fb2daaf01ebf58a8ce29605
will pelham <wp8000@gmail.com>
2017-04-16 15:11:22 -0700

    add n_unique

---
+++ R/n_unique.R
@@ -0,0 +1,13 @@
+#' n_unique
+#'
+#' Function to count number of unique values in a vector.
+#'
+#' @param vector Vector.
+#' @export
+#' @examples n_unique(vector)
+
+#**********************************************************
+n_unique = function(vec) {
+  length(unique(vec))
+}
+#**********************************************************
