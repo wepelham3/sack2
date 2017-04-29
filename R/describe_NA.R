f83cd4597d84b79db083071bd1109055145505f0
will pelham <wp8000@gmail.com>
2017-04-29 16:05:23 -0700

    change name of get_num_NA to describe_NA

--- R/describe_NA.R
+++ R/describe_NA.R
@@ -1,14 +1,14 @@
 #' Return tidy summary of NAs in dataframe
 #'
-#' Function to return a dataframe with description of freuqency of NAs
+#' Function to return a dataframe with description of frequency of NAs
 #' in target dataframe.
 #'
 #' @param data Dataframe.
 #' @export
-#' @examples get_num_NA(mice::boys)
+#' @examples describe_NA(mice::boys)
 
 #**********************************************************
-get_num_NA = function(data) {
+describe_NA = function(data) {
     tibble::data_frame(variable = names(data),
                        count.NA = colSums(is.na(data)),
                        prop.NA = colMeans(is.na(data)) %>% round(2))
