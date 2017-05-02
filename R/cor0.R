8a5487e5920e4959a5cfbbd55b8205660e1eb15e
will pelham <wp8000@gmail.com>
2017-05-02 14:46:10 -0700

    add cor0

---
+++ R/cor0.R
@@ -0,0 +1,36 @@
+#' Create tidy summary of pairwise correlations of vars in a dataframe
+#'
+#' Wraps typical workflow of \code{df \%>\% as.matrix() \%>\% Hmisc::rcorr() \%>\% broom::tidy()}.
+#'
+#' Drops all factors with more than 2 levels.  Converts factors with 2 levels to numerics
+#' via \code{as.numeric(x) - 1}.
+#'
+#' @export
+#' @examples
+#' cor0(mtcars)
+#' cor0(mice::boys)
+
+#**********************************************************
+cor0 = function(data) {
+
+  ncols.valid <- sum(purrr::map_dbl(data, ~ is.numeric(.x) | (is.factor(.x) & nlevels(.x == 2))))
+
+  ncols.dropped = ncol(data) - ncols.valid
+
+  if(ncols.dropped > 0) {
+    warning(paste0("Dropped [", ncols.dropped, "] non-numeric columns with more than two values before computing correlations."))
+  }
+
+  data <- data %>%
+    dplyr::mutate_if(.predicate = function(.x){is.factor(.x) & nlevels(.x == 2)},
+                     .funs = function(.x){as.numeric(.x) - 1}) %>%
+    purrr::keep(is.numeric)
+
+  summary <- data %>%
+    as.matrix() %>%
+    Hmisc::rcorr() %>%
+    broom::tidy()
+
+  return(summary)
+}
+#**********************************************************
