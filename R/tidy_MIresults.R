75eeb46025b501e0bb84ce740446d66d35fe10e4
will pelham <wp8000@gmail.com>
2017-05-06 17:48:22 -0700

    add working example to tidy_MIresults and also change missinfo to numeric

--- R/tidy_MIresults.R
+++ R/tidy_MIresults.R
@@ -5,10 +5,16 @@
 #' @param MIresults (i.e., the output of a \code{mitools::MIcombine} call).
 #' @export
 #' @examples
-#' # non-operative
-#' # with(list.of.imps, glm(y ~ x)) %>%
-#' #    MIcombine() %>%
-#' #    tidy_MIresults()
+#' library(mice)
+#' library(mitools)
+#'
+#' imp <- mice(boys)
+#' imp.long <- complete(imp, action = "long")
+#' imp.list <- imputationList(split(imp.long, imp.long$.imp))
+#'
+#' mi.results <- MIcombine(with(imp.list, glm(hgt ~ wgt)))
+#'
+#' tidy_MIresults(mi.results)
 
 tidy_MIresults = function(MIresults) {
 
@@ -19,8 +25,8 @@ tidy_MIresults = function(MIresults) {
         dplyr::rename(estimate = results,
                       std.error = se,
                       lo = `(lower`,
-                      hi = `upper)`,
-                      miss.info = missInfo) %>%
+                      hi = `upper)`) %>%
+        cbind(miss.info = MIresults$missinfo) %>%
         cbind(df = MIresults$df) %>%
         dplyr::mutate(p.value = 2 * (1 - pt(abs(estimate / std.error), df))) %>%
         dplyr::select(term, estimate, std.error, df, p.value, lo, hi, miss.info)
