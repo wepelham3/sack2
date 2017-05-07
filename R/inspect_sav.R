25d96fc542757c28d70564e20ab1973c2d62eba6
will pelham <wp8000@gmail.com>
2017-05-06 19:51:37 -0700

    add describe option to inspect_sav

--- R/inspect_sav.R
+++ R/inspect_sav.R
@@ -3,43 +3,50 @@
 #' Function that returns dataframe with name, label, values, and value
 #' labels for all variables in an SPSS data file.
 #'
-#' @param path.or.df Either a string indicating location of .sav file or a "labelled" dataframe
+#' @param path.or.data Either a string indicating location of .sav file or a "labelled" dataframe
 #' already imported into R environment.
+#' @param describe Logical indicating whether to append descriptive information on each variable,
+#' in addition to the SPSS metadata. (Calls \code{describe_df()} if so).
 #' @export
 #' @examples
-#' # non-operative
-#' # inspect_sav("my-data.sav")
-#' # inspect_sav(my.df)
-#' # inspect_sav(my.df, view = TRUE)
+#' # SPSS metadata will be blank, since it is an R dataset
+#' library(haven)
+#' write_sav(mtcars, "mtcars.sav")
+#' inspect_sav("mtcars.sav", describe = TRUE)
 
 #**********************************************************
-inspect_sav = function(path.or.df) {
+inspect_sav = function(path.or.data, describe = FALSE) {
 
-    if (is.data.frame(path.or.df)) {
-        labelled.df = path.or.df
-    } else if (file.exists(path.or.df)) {
-        labelled.df = haven::read_sav(path.or.df)
+    if (is.data.frame(path.or.data)) {
+        labelled.data = path.or.data
+    } else if (file.exists(path.or.data)) {
+        labelled.data = haven::read_sav(path.or.data)
     } else {
         stop("Input is neither a valid path nor an existing dataframe in R environment.")
     }
 
-    df = tibble::data_frame(name = names(labelled.df),
-                            label = sjmisc::get_label(labelled.df),
+    data = tibble::data_frame(name = names(labelled.data),
+                            label = sjmisc::get_label(labelled.data),
                             mapping = "",
-                            values = lapply(labelled.df, sjmisc::get_values),
-                            value.labels = sjmisc::get_labels(labelled.df))
+                            values = lapply(labelled.data, sjmisc::get_values),
+                            value.labels = sjmisc::get_labels(labelled.data))
 
     # fill new column mapping values to value labels
-    for (i in 1 : nrow(df)) {
-        if (length(unlist(df$values[i])) == 0 |
-            length(unlist(df$value.labels[i])) == 0) next
-        df$mapping[i] = paste(
-            paste(unlist(df$values[i]),
-                  unlist(df$value.labels[i]), sep = " = "),
+    for (i in 1 : nrow(data)) {
+        if (length(unlist(data$values[i])) == 0 |
+            length(unlist(data$value.labels[i])) == 0) next
+        data$mapping[i] = paste(
+            paste(unlist(data$values[i]),
+                  unlist(data$value.labels[i]), sep = " = "),
             collapse = ", ")
     }
 
-    if (view == FALSE) return(df)
-    else View(df)
+    if (describe == TRUE){
+      data = data %>%
+        dplyr::left_join(describe_df(labelled.data) %>%
+                           dplyr::rename(name = variable))
+    }
+
+    return(data)
 }
 #**********************************************************
