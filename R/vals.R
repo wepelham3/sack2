d2b1f6afa009ec23d82615f88d416a98fc70cb39
will pelham <wp8000@gmail.com>
2017-05-14 16:12:48 -0700

    add cum.prop to vals()

--- R/vals.R
+++ R/vals.R
@@ -3,6 +3,9 @@
 #' Akin to 1-dimensional \code{table} with a few differences:
 #' (1) it returns as a dataframe, and (2) it includes the proportion of elements.
 #'
+#' Note that the \code{cum.prop} column in resulting dataframe was calculated after
+#' sorting based on value.  It will not remain correct if resorted.
+#'
 #' @export
 #' @examples
 #' vals(mtcars$gear)
@@ -18,7 +21,8 @@ vals = function(vector) {
                                  purrr::map_int(value, ~ sum(is.na(vector))),
                                  purrr::map_int(value, ~ sum(vector == .x, na.rm = TRUE))),
                   prop = round(count / length(vector), 2)) %>%
-    dplyr::arrange(value)
+    dplyr::arrange(value) %>%
+    dplyr::mutate(cum.prop = cumsum(prop))
 
   return(df)
 
