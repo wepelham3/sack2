db3174a6b5691d1c36f9fa8cf5bd5f2af4a57d5f
wepelham3 <wp8000@gmail.com>
2017-05-24 22:23:09 -0500

    update vals to be compatible with a vals(df, var) format

--- R/vals.R
+++ R/vals.R
@@ -1,20 +1,39 @@
 #' Create tidy dataframe that summarizes frequency of values in a vector
 #'
 #' Akin to 1-dimensional \code{table} with a few differences:
-#' (1) it returns as a dataframe, and (2) it includes the proportion of elements.
+#' it returns as a dataframe,
+#' it always includes \code{NA}s,
+#' it includes the proportion of elements,
+#' it can take a \code{vals(df, var)} input format for easy chaining.
 #'
 #' Note that the \code{cum.prop} column in resulting dataframe was calculated after
 #' sorting based on value.  It will not remain correct if resorted.
 #'
+#' @param data Either a vector, or a dataframe in which to find \code{var}.
+#' @param var When \code{data} is a dataframe, bare name of column to use.
 #' @export
 #' @examples
+#'
+#' # first input format uses a (df, name) specification like dplyr
+#' vals(mtcars, gear)
+#'
+#' # second input format just takes a vector
 #' vals(mtcars$gear)
 #'
 #' # with NAs
+#' vals(mice::boys, gen)
 #' vals(mice::boys$gen)
 
 #**********************************************************
-vals = function(vector) {
+vals = function(data, var = NULL) {
+
+  if (is.data.frame(data)){
+    vector <- dplyr::select(data, !!rlang::enquo(var)) %>% .[[1]]
+  } else if (is.null(var)){
+    vector <- data
+  } else {
+    stop("Cannot understand inputs.")
+  }
 
   df = data.frame(value = unique(vector)) %>%
     dplyr::mutate(count = ifelse(is.na(value),
@@ -28,6 +47,5 @@ vals = function(vector) {
                      .funs = "round", 2)
 
   return(df)
-
 }
 #**********************************************************
