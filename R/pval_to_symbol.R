8bbfc7adf120fff261742127f2b5946dbbb379cb
wepelham3 <wp8000@gmail.com>
2017-06-07 13:38:26 -0700

    add pval_to_symbol

---
+++ R/pval_to_symbol.R
@@ -0,0 +1,19 @@
+#' Convert p values to symbols
+#'
+#' Converts a vector of p values into character vector of the
+#' symbols ***, **, *, †.
+#'
+#' @param p Vector of p values.
+#' @export
+#' @examples
+#'
+#' pval_to_symbol(c(.50, .0004, .098, .044))
+
+#**********************************************************
+pval_to_symbol = function(p) {
+  ifelse(p < .001, "***",
+         ifelse(p < .01, "**",
+                ifelse(p < .05, "*",
+                       ifelse(p < .10, "†", ""))))
+}
+#**********************************************************
