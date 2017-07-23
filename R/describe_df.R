9f612db37b126036cddfe3000b4ac2869de82d0d
wepelham3 <wp8000@gmail.com>
2017-07-22 17:27:15 -0700

    add nmis column to describe_df()

--- R/describe_df.R
+++ R/describe_df.R
@@ -22,6 +22,7 @@ describe_df = function(data, digits = NULL) {
   # calculate columns that apply to all variables
   results.all <- tibble::data_frame(variable = names(data),
                                     n = colSums(!is.na(data)),
+                                    nmis = colSums(is.na(data)),
                                     distinct = purrr::map(data, unique) %>%
                                       purrr::map(purrr::discard, .p = function(.x){is.na(.x)}) %>%
                                       purrr::map_dbl(length))
@@ -36,10 +37,10 @@ describe_df = function(data, digits = NULL) {
                                         sd = purrr::map_dbl(data.numeric, sd, na.rm = TRUE),
                                         min = purrr::map_dbl(data.numeric, min, na.rm = TRUE),
                                         p10 = purrr::map_dbl(data.numeric, quantile, probs = 0.10, na.rm = TRUE),
-                                        p25 = purrr::map_dbl(data.numeric, quantile, probs = 0.25,  na.rm = TRUE),
+                                        p25 = purrr::map_dbl(data.numeric, quantile, probs = 0.25, na.rm = TRUE),
                                         median = purrr::map_dbl(data.numeric, median, na.rm = TRUE),
-                                        p75 = purrr::map_dbl(data.numeric, quantile, probs = 0.75,  na.rm = TRUE),
-                                        p90 = purrr::map_dbl(data.numeric, quantile, probs = 0.90,  na.rm = TRUE),
+                                        p75 = purrr::map_dbl(data.numeric, quantile, probs = 0.75, na.rm = TRUE),
+                                        p90 = purrr::map_dbl(data.numeric, quantile, probs = 0.90, na.rm = TRUE),
                                         max = purrr::map_dbl(data.numeric, max, na.rm = TRUE))
 
   results <- dplyr::left_join(results.all, results.numeric)
