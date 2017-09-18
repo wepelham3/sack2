95f293af91493dbb77b6088492983d1730bf3c0b
will pelham <wp8000@gmail.com>
2017-09-18 12:11:33 -0700

    switch fit_mice_for_diag to use dots

--- R/fit_mice_for_diag.R
+++ R/fit_mice_for_diag.R
@@ -3,48 +3,48 @@
 #' Function that creates a \code{mice} object for diagnostics, with
 #' \code{m = 5}. Iteratively increases the number of iterations by 5 until
 #' Rhat < 1.05 for mean and variance of all variables in imputation model.
-#' Thus accessing \code{$iteration} in the resulting \code{mice} object
+#' Thus, accessing \code{$iteration} in the resulting \code{mice} object
 #' will give the value of \code{maxit} to be used in the final \code{m > 5}
 #' imputation model.
 #'
 #'See \code{mice::mice} for more documentation of arguments.
 #'
-#' @param data Dataframe to be imputed.
-#' @param pred Square matrix indicating which variables should be used as predictors of which other variables.
-#' @param method Character vector of methods to use for each variable in imputation model.
+#' @param ... Arguments to be passed to \code{mice} call (typically, dataframe, method vector, and prediction matrix).
 #' @param iter10 Logical switch indicating whether function should use the iterative procedure
 #' to find a sufficient number of iterations. When \code{TRUE}, will stop after fitting a \code{m = 5, maxit = 10} model. Included for convenience when working with code
 #' that includes the function.
 #' @export
 #' @examples
 #' library(mice)
-#' pred.matrix <- quickpred(boys, mincor = .10)
-#' df.imp.m5 <- fit_mice_for_diag(boys, pred.matrix, method = c("pmm", "pmm", "pmm", "pmm", "pmm", "pmm", "polr", "pmm", "pmm"))
+#' pred.matrix <- quickpred(boys, mincor = .10, minpuc = 0.50)
+#' df.imp.m5 <- fit_mice_for_diag(data = boys,
+#'                                predictorMatrix = pred.matrix,
+#'                                method = c("pmm", "pmm", "pmm", "pmm", "pmm", "pmm", "polr", "pmm", "pmm"))
 #' df.imp.m5$m
 #' df.imp.m5$iteration
 
 #**********************************************************
-fit_mice_for_diag = function(data, pred, method, iter10 = FALSE) {
+fit_mice_for_diag = function(..., iter10 = FALSE) {
 
     # start with 10 iterations
-    current.maxit = 10
+    current.maxit <- 10
     # initialize max.rhat above threshold of 1.05
-    current.max.rhat = 2
+    current.max.rhat <- 2
 
     while (current.max.rhat > 1.05) {
-        data.imp = mice::mice(data = data, seed = 07191992, m = 5, maxit = current.maxit,
-                              pred = pred, method = method, printFlag = FALSE, diagnostics = TRUE)
+        data.imp <- mice::mice(seed = 07191992, m = 5, maxit = current.maxit,
+                               printFlag = FALSE, diagnostics = TRUE, ...)
 
         # stop running if iter10 switch is on
         if (iter10 == TRUE) return(data.imp)
 
         # if not, continue with statistical checks (Gelman/Rubin)
-        current.max.rhat = miceadds::Rhat.mice(data.imp) %>%
+        current.max.rhat <- miceadds::Rhat.mice(data.imp) %>%
             dplyr::select(Rhat.M.imp, Rhat.Var.imp) %>%
             purrr::map_dbl(max, na.rm = TRUE) %>%
             max()
 
-        current.maxit = current.maxit + 5
+        current.maxit <- current.maxit + 5
     }
 
     cat("----------------------------------")
