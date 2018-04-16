596586dc5ccd8071fe93f15fff3003223b8a1140
wepelham3 <wp8000@gmail.com>
2018-04-15 20:44:59 -0700

    rewrite cor0 and describe_df to respect grouped dataframes

--- R/cor0.R
+++ R/cor0.R
@@ -1,13 +1,16 @@
 #' Create tidy summary of pairwise correlations of vars in a dataframe
 #'
-#' Wraps typical workflow of \code{df \%>\% as.matrix() \%>\% Hmisc::rcorr() \%>\% broom::tidy()}.
+#' Wraps typical workflow of
+#' \code{df \%>\% as.matrix() \%>\% Hmisc::rcorr() \%>\% broom::tidy()}.
+#' Respects grouped dataframes a la \code{dplyr}.
 #'
 #' Drops all factors with more than 2 levels and throws a warning.
 #' Silently converts factors with 2 levels to numerics via \code{as.numeric(x) - 1}.
 #'
-#' @param data Dataframe.
-#' @param pattern Optional vector of patterns to match for (i.e., filter for) in the resulting
-#' two columns with variable names. Useful if only interested in correlations with a specific variable (e.g., the outcome).
+#' @param .data Dataframe.
+#' @param pattern Optional vector of patterns to match for (i.e., filter for) in
+#' the resulting two columns with variable names. Useful if only interested in
+#' correlations with a specific variable (e.g., the outcome).
 #' @export
 #' @examples
 #' cor0(mtcars)
@@ -20,34 +23,93 @@
 #' cor0(mtcars, c("mpg", "cyl"))
 
 #**********************************************************
-cor0 = function(data, pattern = NULL) {
+cor0 = function(.data, pattern = NULL) {
 
-  cols.to.drop <- purrr::discard(data, ~ is.numeric(.x) | (is.factor(.x) & nlevels(.x) == 2)) %>%
+  
+  .groups <- NULL
+  
+  if (dplyr::is_grouped_df(.data) == TRUE){
+    
+    .groups <- dplyr::groups(.data)
+    
+    warning("describe_df respected the following grouping vars: ",
+            paste0(.groups, collapse = ", "))
+    
+  }
+  
+  # drop columns that are non-numeric
+  
+  cols.to.drop <- purrr::discard(.data,
+                                 ~ is.numeric(.x) | (is.factor(.x) & nlevels(.x) == 2)) %>%
     names()
+  
+  cols.to.drop <- setdiff(cols.to.drop, .groups)
 
   ncols.dropped <- length(cols.to.drop)
 
   if(ncols.dropped > 0) {
+    
     warning(paste0("Dropped ", ncols.dropped, " non-numeric columns with more than two values before computing correlations:\n    ",
                    paste0(cols.to.drop, collapse = ", ")))
+  
   }
 
-  data <- data %>%
-    dplyr::mutate_if(.predicate = function(.x){is.factor(.x) & nlevels(.x) == 2},
-                     .funs = function(.x){as.numeric(.x) - 1}) %>%
-    purrr::keep(is.numeric)
+  # recode any binary factors to 0/1 numerics
+  
+  cols.to.binarize <- keep(.data,
+                           ~ is.factor(.x) & nlevels(.x) == 2) %>% 
+    names() %>% 
+    setdiff(.groups)
+  
+  .data <- .data %>%
+    dplyr::mutate_at(cols.to.binarize,
+                     ~ as.numeric(.x) - 1)
+  
+  # define internal function that gets the tidy correlation table
+  
+  get_correlations = function(.data){
+    
+    .data.numeric <- .data %>%
+      ungroup() %>% 
+      select_if(is.numeric)
+    
+    if (ncol(.data.numeric) != ncol(.data) - ncols.dropped - length(.groups)) {
+      
+      stop("Fewer columns than expected were passed to Hmisc::rcorr().")
+      
+    }
+    
+    .data.numeric %>% 
+      as.matrix() %>%
+      Hmisc::rcorr() %>%
+      broom::tidy() 
+    
+  }
+  
+  # apply internal function either by group or to single dataframe
+  
+  if (dplyr::is_grouped_df(.data) == TRUE){
+    
+    results <- dplyr::do(.data,
+                         get_correlations(.)) %>%
+      dplyr::ungroup()
 
-  summary <- data %>%
-    as.matrix() %>%
-    Hmisc::rcorr() %>%
-    broom::tidy()
+  } else {
+    
+    results <- get_correlations(.data)
+    
+  }
+  
+  # filter results based on pattern argument
+  
+  if (! is.null(pattern)) {
 
-  if (!is.null(pattern)) {
-    summary = summary %>%
+    results <- results %>%
       dplyr::filter(stringr::str_detect(column1, paste(pattern, collapse = "|")) |
                     stringr::str_detect(column2, paste(pattern, collapse = "|")))
+
   }
 
-  return(summary)
+  results
 }
 #**********************************************************
--- R/describe_df.R
+++ R/describe_df.R
@@ -2,6 +2,7 @@
 #'
 #' Function to return a dataframe with descriptives for a target dataframe.
 #' Each row in the returned dataframe will correspond to a column in the original dataframe.
+#' Respects grouped dataframes a la \code{dplyr}.
 #'
 #' Note that the count in the \code{distinct} column excludes values of \code{NA}.
 #'
@@ -18,37 +19,69 @@
 
 #**********************************************************
 describe_df = function(data, digits = NULL) {
-
-  # calculate columns that apply to all variables
-  results.all <- tibble::data_frame(variable = names(data),
-                                    n = colSums(!is.na(data)),
-                                    nmis = colSums(is.na(data)),
-                                    distinct = purrr::map(data, unique) %>%
-                                      purrr::map(purrr::discard, .p = function(.x){is.na(.x)}) %>%
-                                      purrr::map_dbl(length))
-
-
-  data.numeric <- data %>%
-    purrr::keep(is.numeric)
-
-  # calculate columns that ONLY apply to numeric variables
-  results.numeric <- tibble::data_frame(variable = names(data.numeric),
-                                        mean = purrr::map_dbl(data.numeric, mean, na.rm = TRUE),
-                                        sd = purrr::map_dbl(data.numeric, sd, na.rm = TRUE),
-                                        min = purrr::map_dbl(data.numeric, min, na.rm = TRUE),
-                                        p10 = purrr::map_dbl(data.numeric, quantile, probs = 0.10, na.rm = TRUE),
-                                        p25 = purrr::map_dbl(data.numeric, quantile, probs = 0.25, na.rm = TRUE),
-                                        median = purrr::map_dbl(data.numeric, median, na.rm = TRUE),
-                                        p75 = purrr::map_dbl(data.numeric, quantile, probs = 0.75, na.rm = TRUE),
-                                        p90 = purrr::map_dbl(data.numeric, quantile, probs = 0.90, na.rm = TRUE),
-                                        max = purrr::map_dbl(data.numeric, max, na.rm = TRUE))
-
-  results <- dplyr::left_join(results.all, results.numeric)
-
-  if (!is.null(digits)){
+  
+  if (!is.data.frame(data)) stop("First argument is not a dataframe.")
+  
+  # define internal function that summarizes a given dataframe
+  
+  get_description = function(data){
+    
+    # calculate columns that apply to all variables
+    
+    results.all <-
+      tibble::data_frame(variable = names(data),
+                         n = colSums(!is.na(data)),
+                         nmis = colSums(is.na(data)),
+                         distinct = purrr::map(data, unique) %>%
+                           purrr::map(~ purrr::discard(.x, is.na)) %>%
+                           purrr::map_dbl(length))
+    
+    
+    data.numeric <- data %>%
+      purrr::keep(is.numeric)
+    
+    # calculate columns that ONLY apply to numeric variables
+    
+    results.numeric <-
+      tibble::data_frame(variable = names(data.numeric),
+                         mean = purrr::map_dbl(data.numeric, mean, na.rm = TRUE),
+                         sd = purrr::map_dbl(data.numeric, sd, na.rm = TRUE),
+                         min = purrr::map_dbl(data.numeric, min, na.rm = TRUE),
+                         p10 = purrr::map_dbl(data.numeric, quantile, probs = 0.10, na.rm = TRUE),
+                         p25 = purrr::map_dbl(data.numeric, quantile, probs = 0.25, na.rm = TRUE),
+                         median = purrr::map_dbl(data.numeric, median, na.rm = TRUE),
+                         p75 = purrr::map_dbl(data.numeric, quantile, probs = 0.75, na.rm = TRUE),
+                         p90 = purrr::map_dbl(data.numeric, quantile, probs = 0.90, na.rm = TRUE),
+                         max = purrr::map_dbl(data.numeric, max, na.rm = TRUE))
+    
+    dplyr::left_join(results.all, results.numeric)
+    
+  }
+  
+  # call internal function within do() if a grouped dataframe
+  
+  if (dplyr::is_grouped_df(data) == TRUE){
+    
+    .groups <- dplyr::groups(data)
+    
+    warning("describe_df respected the following grouping vars: ",
+            paste0(.groups, collapse = ", "))
+    
+    results <- dplyr::do(data,
+                         get_description(.)) %>%
+      dplyr::ungroup() %>%
+      dplyr::filter(! variable %in% .groups)
+    
+  } else {
+    
+    results <- get_description(data)
+    
+  }
+  
+  if (! is.null(digits)){
     results <- sack2::round0(results, digits)
   }
-
-  return(results)
+  
+  results
 }
 #**********************************************************
