1ca22573e09130aaf2a70923f85d89a427885107
William Pelham <wpelham@sundevils.asu.edu>
2018-04-30 15:49:45 -0700

    adjust describe_df to binarize binary factors

--- R/describe_df.R
+++ R/describe_df.R
@@ -19,15 +19,40 @@
 
 #**********************************************************
 describe_df = function(data, digits = NULL) {
-  
+
   if (!is.data.frame(data)) stop("First argument is not a dataframe.")
-  
+
+  .groups <- NULL
+
+  if (dplyr::is_grouped_df(data) == TRUE){
+
+    .groups <- dplyr::groups(data)
+
+    warning("describe_df respected the following grouping vars: ",
+            paste0(.groups, collapse = ", "))
+
+  }
+
+  # recode any binary factors to 0/1 numerics
+
+  cols.to.binarize <- keep(data,
+                           ~ is.factor(.x) & nlevels(.x) == 2) %>%
+    names() %>%
+    setdiff(.groups)
+
+  data <- data %>%
+    dplyr::mutate_at(cols.to.binarize,
+                     ~ as.numeric(.x) - 1)
+
+  warning("describe_df binarized the following vars: ",
+          paste0(cols.to.binarize, collapse = ", "))
+
   # define internal function that summarizes a given dataframe
-  
+
   get_description = function(data){
-    
+
     # calculate columns that apply to all variables
-    
+
     results.all <-
       tibble::data_frame(variable = names(data),
                          n = colSums(!is.na(data)),
@@ -35,13 +60,13 @@ describe_df = function(data, digits = NULL) {
                          distinct = purrr::map(data, unique) %>%
                            purrr::map(~ purrr::discard(.x, is.na)) %>%
                            purrr::map_dbl(length))
-    
-    
+
+
     data.numeric <- data %>%
       purrr::keep(is.numeric)
-    
+
     # calculate columns that ONLY apply to numeric variables
-    
+
     results.numeric <-
       tibble::data_frame(variable = names(data.numeric),
                          mean = purrr::map_dbl(data.numeric, mean, na.rm = TRUE),
@@ -53,35 +78,30 @@ describe_df = function(data, digits = NULL) {
                          p75 = purrr::map_dbl(data.numeric, quantile, probs = 0.75, na.rm = TRUE),
                          p90 = purrr::map_dbl(data.numeric, quantile, probs = 0.90, na.rm = TRUE),
                          max = purrr::map_dbl(data.numeric, max, na.rm = TRUE))
-    
+
     dplyr::left_join(results.all, results.numeric)
-    
+
   }
-  
+
   # call internal function within do() if a grouped dataframe
-  
+
   if (dplyr::is_grouped_df(data) == TRUE){
-    
-    .groups <- dplyr::groups(data)
-    
-    warning("describe_df respected the following grouping vars: ",
-            paste0(.groups, collapse = ", "))
-    
+
     results <- dplyr::do(data,
                          get_description(.)) %>%
       dplyr::ungroup() %>%
       dplyr::filter(! variable %in% .groups)
-    
+
   } else {
-    
+
     results <- get_description(data)
-    
+
   }
-  
+
   if (! is.null(digits)){
     results <- sack2::round0(results, digits)
   }
-  
+
   results
 }
 #**********************************************************
