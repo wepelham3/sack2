1f200952ed19e8d99ce712d3282189addaaa154c
William Pelham <wpelham@sundevils.asu.edu>
2018-05-01 11:01:15 -0700

    return Rhat data in fit_mice_for_diag()

--- R/fit_mice_for_diag.R
+++ R/fit_mice_for_diag.R
@@ -7,37 +7,47 @@
 #' will give the value of \code{maxit} to be used in the final \code{m > 5}
 #' imputation model.
 #'
-#'See \code{mice::mice} for more documentation of arguments.
+#' Always uses same random seed. Includes a dataframe with all Rhat values at
+#' each step, which can be accessessed via \code{mice$rhat}. See \code{mice::mice}
+#' for more documentation of arguments.
 #'
-#' @param ... Arguments to be passed to \code{mice} call (typically, dataframe, method vector, and prediction matrix).
-#' @param iter10 Logical switch indicating whether function should use the iterative procedure
-#' to find a sufficient number of iterations. When \code{TRUE}, will stop after fitting a \code{m = 5, maxit = 10} model. Included for convenience when working with code
-#' that includes the function.
+#' @param ... Arguments to be passed to \code{mice} call (typically, dataframe,
+#' method vector, and prediction matrix).
+#' @param iter10 Logical switch indicating whether function should use the
+#' iterative procedure to find a sufficient number of iterations. When
+#' \code{TRUE}, will stop after fitting a \code{m = 5, maxit = 10} model.
+#' Included for convenience when working with code that includes the function.
 #' @export
 #' @examples
 #' library(mice)
 #' pred.matrix <- quickpred(boys, mincor = .10, minpuc = 0.50)
-#' df.imp.m5 <- fit_mice_for_diag(data = boys,
-#'                                predictorMatrix = pred.matrix,
-#'                                method = c("pmm", "pmm", "pmm", "pmm", "pmm", "pmm", "polr", "pmm", "pmm"))
-#' df.imp.m5$m
-#' df.imp.m5$iteration
+#' mice.m5 <- fit_mice_for_diag(data = boys,
+#'                              predictorMatrix = pred.matrix,
+#'                              method = c("pmm"))
+#' mice.m5$m
+#' mice.m5$iteration
+#' mice.m5$rhat
 
 #**********************************************************
 fit_mice_for_diag = function (..., iter10 = FALSE){
 
   current.maxit <- 10
 
-  current.max.rhat <- 999
+  current.max.Rhat <- 999
 
-  while (current.max.rhat > 1.05) {
+  df.Rhats <- tibble::data_frame()
+
+  while (current.max.Rhat > 1.05) {
 
     .mice <- mice::mice(seed = 7191992, m = 5, maxit = current.maxit,
                         printFlag = FALSE, diagnostics = TRUE, ...)
 
     if (iter10 == TRUE) return(.mice)
 
-    current.max.rhat <- miceadds::Rhat.mice(.mice) %>%
+    Rhats <- miceadds::Rhat.mice(.mice) %>%
+      cbind(maxiter = current.maxit)
+
+    current.max.Rhat <- Rhats %>%
       dplyr::select(Rhat.M.imp, Rhat.Var.imp) %>%
       purrr::map_dbl(max, na.rm = TRUE) %>%
       max()
@@ -46,13 +56,18 @@ fit_mice_for_diag = function (..., iter10 = FALSE){
     cat("\n")
     cat(paste0("Finished running maxiter of ...", current.maxit))
     cat("\n")
-    cat(paste0("This yielded a max(Rhat) of... ", round(current.max.rhat, 2)))
+    cat(paste0("This yielded a max(Rhat) of... ", round(current.max.Rhat, 2)))
     cat("\n")
     cat("----------------------------------")
     cat("\n")
 
+    df.Rhats <- df.Rhats %>%
+      dplyr::bind_rows(Rhats)
+
     current.maxit <- current.maxit + 5
   }
+
+  .mice$rhat <- df.Rhats
 
   .mice
 }
