272c421acf6b2be7647ba4c7bbc582130edda260
William Pelham <wpelham@sundevils.asu.edu>
2019-04-02 12:28:00 -0700

    improve source_lines()

--- R/source_lines.R
+++ R/source_lines.R
@@ -1,14 +1,23 @@
 #' Source specific lines in an R file
 #'
-#' From https://gist.github.com/christophergandrud/1eb4e095974204b12af9.
-#'
 #' @param file character string with the path to the file to source.
-#' @param lines numeric vector of lines to source in \code{file}.
+#' @param start row number of first line in \code{file} to source.
+#' @param end row number of last line in \code{file} to source.
 #' @export
 #' @examples
 #' # non-operative
-#' # source_lines("load-data.R", 1 : 20)
+#' # source_lines("load-data.R", start = 50)
 
-source_lines <- function(file, lines){
-    source(textConnection(readLines(file)[lines]))
+source_lines = function (file, start = NULL, end = NULL){
+
+  if (length(start) > 1) stop("Length of start > 1.")
+  if (length(end) > 1) stop("Length of end > 1.")
+
+  lines <- readLines(file)
+
+  if (is.null(start)) start <- 1
+
+  if (is.null(end)) end <- length(lines)
+
+  source(textConnection(lines[start:end]))
 }
