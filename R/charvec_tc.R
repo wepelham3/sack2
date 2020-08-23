1773cd5f692a9147103c0ce3a9adc58602a3bc43
wepelham3 <wp8000@gmail.com>
2020-08-22 20:49:03 -0700

    add args to charvec_tc()

--- R/charvec_tc.R
+++ R/charvec_tc.R
@@ -1,25 +1,45 @@
-#' Copy character vector to clipboard as string of collapsed values for pasting
+#' Copy character vector to clipboard for pasting
 #'
-#' Takes a character vector, quotes each element and concatenates them, then
-#' copies the collapsed string to clipboard.
+#' Concatenate the elements of a character vector and copy
+#' the collapsed string to the clipboard.
 #'
-#' Fast way to create code like \code{c("element1", "element2", ...)}
+#' Fast way to create text like \code{c("element1", "element2", ...)}
 #' for pasting into code.
 #'
 #' @param x Character vector
+#' @param quote Put quotation marks around elements?
+#' @param linebreaks Put linebreaks after each element?
 #' @export
-#' @return A string of quoted elements of \code{x}, separated by
-#' commas and linebreaks.
+#' @return Nothing - copies result to clipboard.
 #' @examples
-#' charvec_tc(c("asdjalksjdlaksjd", "12312093u10923012", "asd"))
+#' x <- c("asdjalksjdlaksjd", "12312093u10923012", "asd")
+#' charvec_tc(x)
+#' charvec_tc(x, quote = FALSE, linebreaks = TRUE)
+#' charvec_tc(x, quote = TRUE, linebreaks = FALSE)
+#' charvec_tc(x, quote = FALSE, linebreaks = FALSE)
 
 #**********************************************************
-charvec_tc <- function(x){
+charvec_tc <- function(x, quote = TRUE, linebreaks = TRUE){
 
   if ( ! is.character(x)) stop("x is not of type character.")
 
-  x <- paste0('"', x, '"')
-  x <- paste0(x, collapse = ",\n")
+  if (quote == TRUE){
+
+    x <- paste0("\"", x, "\"")
+
+  }
+
+  if (linebreaks == TRUE){
+
+    x <- paste0(x, collapse = ",\n")
+
+  }
+
+  if (linebreaks == FALSE){
+
+    x <- paste0(x, collapse = ", ")
+
+  }
 
   clipr::write_clip(x)
 }
