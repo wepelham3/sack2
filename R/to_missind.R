9437a35a5516ebbaee6ee1649a7a1cdae80f99db
wepelham3 <wp8000@gmail.com>
2020-10-09 10:51:30 -0700

    improve doc for to_missind()

--- R/to_missind.R
+++ R/to_missind.R
@@ -1,7 +1,18 @@
 #' Recode a target column in dataframe to indicator of missingness
 #'
+#' @description
 #' Returns the input dataframe with the target column recoded such that
 #' 0 = missing and 1 = present.
+#'
+#' To facilitate a common task that would otherwise require a pattern like this:
+#'
+#' \code{
+#' df \%>\%
+#'   mutate(x = ifelse( ! is.na(x), 1, 0)) \%>\%
+#'   count(x)
+#' }
+#'
+#' @details Note that despite the name "missingness indicator", a value of 1 = present.
 #'
 #' @param data Dataframe
 #' @param x String indicating name of target column
