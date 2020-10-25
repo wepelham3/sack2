63d907e2e63fb82afa5b11e99e1fc1c57da86c87
wepelham3 <wp8000@gmail.com>
2020-10-24 17:32:18 -0700

    add marginal argument for pval_to_symbol()

--- R/pval_to_symbol.R
+++ R/pval_to_symbol.R
@@ -4,15 +4,31 @@
 #' symbols ***, **, *, †.
 #'
 #' @param p Vector of p values.
+#' @param marginal boolean. Should include dagger for p < .10?
 #' @export
 #' @examples
 #' pval_to_symbol(c(.50, .0004, .098, .044))
+#' pval_to_symbol(c(.50, .0004, .098, .044), marginal = FALSE)
 
 #**********************************************************
-pval_to_symbol = function(p) {
-  ifelse(p < .001, "***",
-         ifelse(p < .01, "**",
-                ifelse(p < .05, "*",
-                       ifelse(p < .10, "†", ""))))
+pval_to_symbol <- function(p, marginal = TRUE){
+
+  if (marginal == TRUE){
+
+    p <- ifelse(p < .001, "***",
+                ifelse(p < .01, "**",
+                       ifelse(p < .05, "*",
+                              ifelse(p < .10, "†", ""))))
+
+  }
+
+  if (marginal == FALSE){
+
+    p <- ifelse(p < .001, "***",
+                ifelse(p < .01, "**",
+                       ifelse(p < .05, "*", "")))
+  }
+
+  p
 }
 #**********************************************************
