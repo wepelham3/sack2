356781a57711e21e9da11f06bbbf60e0fe386fa6
wepelham3 <wp8000@gmail.com>
2021-02-23 14:42:17 -0800

    generalize lp() to handle data.frame()

--- R/lp.R
+++ R/lp.R
@@ -1,20 +1,33 @@
-#' Print tibble with all rows shown
+#' Print dataframe with all rows shown
 #'
-#' *L*ong *p*rint to override standard tibble behavior.
+#' *L*ong *p*rint to override standard data.frame() and tibble() printing behavior.
 #'
 #' Functionally, an alternative to \code{View}.
 #'
+#' @param x \code{data.frame()} or \code{tibble()}
 #' @export
 #' @examples
-#' tibble <- tibble::as_tibble(mtcars)
 #'
-#' lp(tibble)
+#' x <- do.call(rbind, replicate(20, as.matrix(mtcars), simplify = FALSE))
 #'
-#' # cf. suppression behavior
-#' print(tibble)
-
+#' x <- as.data.frame(x)
+#'
+#' print(x)
+#' lp(x)
+#'
+#' x <- tibble::as_tibble(x)
+#'
+#' print(x)
+#' lp(x)
+#'
 #**********************************************************
-lp = function(tibble) {
-  print(tibble, n = 10000)
+lp <- function(x){
+
+  if (tibble::is_tibble(x)) print(x, n = 1e5)
+
+  else if (is.data.frame(x)) print(x, max = 1e5)
+
+  else stop("x is neither data.frame() nor tibble()")
+
 }
 #**********************************************************
