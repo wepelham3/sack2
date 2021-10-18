59c81d9a9e378b974ab5a9d90ef1cd72ef2f7c88
wepelham3 <wp8000@gmail.com>
2021-10-18 13:02:02 -0700

    add switch to pval_to_jama

--- R/pval_to_jama.R
+++ R/pval_to_jama.R
@@ -4,17 +4,27 @@
 #' JAMA journal standards for the reporting of p values.
 #'
 #' @param p Vector of p values.
+#' @param includePequals Include "p = .XX"?
 #' @export
 #' @examples
 #' pval_to_jama(c(.50, .0004, .000000001, .098, .044, .90))
 #**********************************************************
-pval_to_jama <- function(p){
+pval_to_jama <- function(p, includePequals = FALSE){
 
   out <- dplyr::case_when(p < .001 ~ "<.001",
                           p < .01 ~ sprintf("%0.3f", p),
                           p >= .01 ~ sprintf("%0.2f", p))
 
-  stringr::str_replace(out, "^0\\.", ".")
+  out <- stringr::str_replace(out, "^0\\.", ".")
+
+  if (includePequals == TRUE){
+
+    out <- dplyr::case_when(stringr::str_detect(out, "<") ~ paste0("p", out),
+                            TRUE ~ paste0("p=", out))
+
+  }
+
+  out
 
 }
 #**********************************************************
